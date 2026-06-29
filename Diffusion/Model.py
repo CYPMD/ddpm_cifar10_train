5184070159a3d5fb07711b90771ad5cc7c72f84c
CYPMD <demirel.mehmet@ucy.ac.cy>
2026-06-29 11:14:59 +0300

    Update Model.py

--- Diffusion/Model.py
+++ Diffusion/Model.py
@@ -1,5 +1,3 @@
-
-   
 import math
 import torch
 from torch import nn
@@ -234,15 +232,3 @@ class UNet(nn.Module):
 
         assert len(hs) == 0
         return h
-
-
-if __name__ == '__main__':
-    batch_size = 8
-    model = UNet(
-        T=1000, ch=128, ch_mult=[1, 2, 2, 2], attn=[1],
-        num_res_blocks=2, dropout=0.1)
-    x = torch.randn(batch_size, 3, 32, 32)
-    t = torch.randint(1000, (batch_size, ))
-    y = model(x, t)
-    print(y.shape)
-
