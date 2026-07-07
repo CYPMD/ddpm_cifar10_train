3276d063e914620f15e0ee4a569c310f4b534ffa
CYPMD <demirel.mehmet@ucy.ac.cy>
2026-07-07 10:43:23 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,17 +3,17 @@ from Diffusion.Train import train, eval
 
 def main(model_config=None):
     modelConfig = {
-        "state": "train", # or "eval"
-        "training_steps": 800000,     # CIFAR-10 paper trained for 800k steps exactly
-        "save_weight_interval": 50000,# Save weights every 50k steps to save disk space
-        "batch_size": 128,            # Match paper
+        "state": "eval", # or "eval" "train"
+        "training_steps": 100000,     # CIFAR-10 paper trained for 800k steps exactly
+        "save_weight_interval": 10000,# Save weights every 50k steps to save disk space
+        "batch_size": 1024,           # Match paper
         "T": 1000,
         "channel": 128,
         "channel_mult": [1, 2, 2, 2], # Match paper
         "attn": [1],                  # Attention at 16x16 resolution
         "num_res_blocks": 2,
         "dropout": 0.1,               # Match paper for CIFAR-10
-        "lr": 2e-4,                   # Constant learning rate from the paper
+        "lr": 5e-4,                   # Constant learning rate from the paper
         "beta_1": 1e-4,
         "beta_T": 0.02,
         "img_size": 32,
@@ -22,10 +22,10 @@ def main(model_config=None):
         "device": "cuda:0", 
         "training_load_weight": None, 
         "save_weight_dir": "./Checkpoints/",
-        "test_load_weight": "ckpt_ema_800000.pt", # Evaluate on the final EMA weights
+        "test_load_weight": "ckpt_ema_40000.pt", # Evaluate on the final EMA weights
         "sampled_dir": "./SampledImgs/",
-        "sampledNoisyImgName": "NoisyNoGuidenceImgs.png",
-        "sampledImgName": "SampledNoGuidenceImgs.png",
+        "sampledNoisyImgName": "NoisyNoGuidenceImgs_ema_40000.png",
+        "sampledImgName": "SampledNoGuidenceImgs_ema_40000.png",
         "nrow": 8
     }
     if model_config is not None:
